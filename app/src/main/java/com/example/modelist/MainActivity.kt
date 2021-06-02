229b38b2233fab9ddba39a1fb95a889d1e21d23f
akhyari <muhammadraffi.akhyari@gmail.com>
2021-06-02 14:51:35 +0700

    + Action Bar sesuai Title

--- app/src/main/java/com/example/modelist/MainActivity.kt
+++ app/src/main/java/com/example/modelist/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +13,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var rvHeroes: RecyclerView
     private var list: ArrayList<Hero> = arrayListOf()
+    private var title: String = "Mode List"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBarTitle(title)
 
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
@@ -59,16 +62,26 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode){
             R.id.action_list -> {
+                title = "Mode List"
                 showRecyclerList()
             }
 
             R.id.action_grid -> {
+                title = "Mode Grid"
                 showRecyclerGrid()
             }
 
             R.id.action_cardview -> {
+                title = "Mode CardView"
                 showRecyclerCardView()
             }
+        }
+        setActionBarTitle(title)
+    }
+
+    private fun setActionBarTitle(title: String) {
+        if(supportActionBar != null){
+            (supportActionBar as ActionBar).title = title
         }
     }
 
